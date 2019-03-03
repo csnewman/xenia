2f59babef871c1ff97c1da0c957e10150c2c9497
Chandler Newman <chandler2newman@hotmail.co.uk>
2019-03-03 00:50:36 +0000

    Disable protobuf warnings

--- third_party/protobuf/src/google/protobuf/wire_format_lite.h
+++ third_party/protobuf/src/google/protobuf/wire_format_lite.h
@@ -53,6 +53,9 @@
 #define GOOGLE_PROTOBUF_UTF8_VALIDATION_ENABLED
 #endif
 
+#pragma warning(disable : 4700)
+#pragma warning(disable : 4244)
+
 // Avoid conflict with iOS where <ConditionalMacros.h> #defines TYPE_BOOL.
 //
 // If some one needs the macro TYPE_BOOL in a file that includes this header,
